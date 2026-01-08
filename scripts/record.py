c8adb333081148f1c0a48963d29fe812bed51143
Jack Kelly <jack@openclimatefix.org>
2026-01-08 11:15:10 +0000

    Refactoring: Moving into EnvoyRecorder class. And move into a file in the library.

--- scripts/record.py
+++ scripts/record.py
@@ -1,93 +1,12 @@
-import json
-import time
-from pathlib import Path
-from typing import TypedDict
-
-import requests
-import urllib3
-from requests import Response
-
-from envoy_recorder.config_loader import EnvoyRecorderConfig
+from envoy_recorder.envoy_recorder import EnvoyRecorder
 from envoy_recorder.logging import get_logger
 
 log = get_logger(__name__)
 
-
-class WrappedEnvoyData(TypedDict):
-    """Wrap the Envoy data in a new JSON object that includes the retrieval
-    time, to make it trivial to decide when to rotate the filenames."""
-
-    retrieval_time: int  # Unix timestamp in seconds
-    data: str  # The raw Enphase JSON
-
-
-def fetch_data_from_envoy(config: EnvoyRecorderConfig) -> WrappedEnvoyData:
-    # Disable SSL Warnings because the Envoy uses self-signed certs.
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-    token = config.envoy.token
-    ip_address = config.envoy.ip_address
-    headers = {"Authorization": f"Bearer {token}"}
-    retrieval_time = round(time.time())
-    url = f"http://{ip_address}/ivp/pdm/device_data"
-
-    log.debug("Fetching data from %s...", url)
-    response: Response = requests.get(url, headers=headers, verify=False, timeout=10)
-    response.raise_for_status()
-    data: str = response.text
-    data = data.strip()
-    log.debug("Successfully retrieved data from %s.", url)
-    log.debug("First 50 characters of response text: '%s'", data[:50])
-
-    return {"retrieval_time": retrieval_time, "data": data}
-
-
-def save(config: EnvoyRecorderConfig, data: WrappedEnvoyData):
-    cache_dir: Path = config.paths.cache_dir
-    if not cache_dir.exists():
-        log.info(
-            "Cache directory %s does not exist yet, so we will create it now.",
-            cache_dir,
-        )
-        cache_dir.mkdir(parents=True)
-    live_file: Path = config.paths.live_file
-    log.debug("Appending JSON data to %s", live_file)
-    json_string = json.dumps(data)
-    with open(live_file, "a") as f:
-        f.write(json_string + "\n")
-
-
-def first_retrieval_time_in_jsonl_file(live_file: Path) -> int:
-    with live_file.open(mode="r") as f:
-        first_line = f.readline()
-    first_line_dict: WrappedEnvoyData = json.loads(first_line)
-    return first_line_dict["retrieval_time"]
-
-
-def rotate_if_necessary(config: EnvoyRecorderConfig):
-    live_file = config.paths.live_file
-    retrieval_time = first_retrieval_time_in_jsonl_file(live_file)
-    age_in_seconds = round(time.time()) - retrieval_time
-    log.debug("Age of %s is %d seconds", live_file, age_in_seconds)
-    assert age_in_seconds >= 0
-    age_of_live_file_in_minutes = round(age_in_seconds / 60)
-
-    if age_of_live_file_in_minutes > config.intervals.rotate_minutes:
-        archive_filename = config.paths.cache_dir / f"archive_{retrieval_time}.jsonl"
-        log.info("Moving %s to %s", live_file, archive_filename)
-        live_file.rename(archive_filename)
-
-
-def main() -> None:
-    config = EnvoyRecorderConfig.load()
-    envoy_data = fetch_data_from_envoy(config)
-    save(config, envoy_data)
-    rotate_if_necessary(config)
-
-
 if __name__ == "__main__":
     try:
-        main()
+        envoy_recorder = EnvoyRecorder()
+        envoy_recorder.run()
     except:
         log.exception("Exception raised in main()!")
         raise
---
+++ src/envoy_recorder/envoy_recorder.py
@@ -0,0 +1,93 @@
+import json
+import time
+from pathlib import Path
+from typing import TypedDict
+
+import requests
+import urllib3
+from requests import Response
+
+from envoy_recorder.config_loader import EnvoyRecorderConfig
+from envoy_recorder.logging import get_logger
+
+log = get_logger(__name__)
+
+
+class WrappedEnvoyData(TypedDict):
+    """Wrap the Envoy data in a new JSON object that includes the retrieval
+    time, to make it trivial to decide when to rotate the filenames."""
+
+    retrieval_time: int  # Unix timestamp in seconds
+    data: str  # The raw Enphase JSON
+
+
+class EnvoyRecorder:
+    def __init__(self) -> None:
+        self.config = EnvoyRecorderConfig.load()
+
+    def run(self) -> None:
+        envoy_data = self.fetch_data_from_envoy()
+        self.save(envoy_data)
+        if self.live_file_is_old_enough_for_conversion():
+            new_filename = self.rename_live_file()
+            append_to_parquet(new_filename)
+
+    def fetch_data_from_envoy(self) -> WrappedEnvoyData:
+        # Disable SSL Warnings because the Envoy uses self-signed certs.
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+        token = self.config.envoy.token
+        ip_address = self.config.envoy.ip_address
+        headers = {"Authorization": f"Bearer {token}"}
+        retrieval_time = round(time.time())
+        url = f"http://{ip_address}/ivp/pdm/device_data"
+
+        log.debug("Fetching data from %s...", url)
+        response: Response = requests.get(
+            url, headers=headers, verify=False, timeout=10
+        )
+        response.raise_for_status()
+        data: str = response.text
+        data = data.strip()
+        log.debug("Successfully retrieved data from %s.", url)
+        log.debug("First 50 characters of response text: '%s'", data[:50])
+
+        return {"retrieval_time": retrieval_time, "data": data}
+
+    def save(self, data: WrappedEnvoyData):
+        cache_dir: Path = self.config.paths.cache_dir
+        if not cache_dir.exists():
+            log.info(
+                "Cache directory %s does not exist yet, so we will create it now.",
+                cache_dir,
+            )
+            cache_dir.mkdir(parents=True)
+        live_file: Path = self.config.paths.live_file
+        log.debug("Appending JSON data to %s", live_file)
+        json_string = json.dumps(data)
+        with open(live_file, "a") as f:
+            f.write(json_string + "\n")
+
+    def live_file_is_old_enough_for_conversion(self) -> bool:
+        live_file = self.config.paths.live_file
+        retrieval_time = first_retrieval_time_in_jsonl_file(live_file)
+        age_in_seconds = round(time.time()) - retrieval_time
+        log.debug("Age of %s is %d seconds", live_file, age_in_seconds)
+        assert age_in_seconds >= 0
+        age_of_live_file_in_minutes = round(age_in_seconds / 60)
+        return age_of_live_file_in_minutes > self.config.intervals.rotate_minutes
+
+    def rename_live_file(self) -> Path:
+        old_filename = self.config.paths.live_file
+        cache_dir = self.config.paths.cache_dir
+        retrieval_time = first_retrieval_time_in_jsonl_file(old_filename)
+        new_filename = cache_dir / f"processing_{retrieval_time}.jsonl"
+        log.info("Moving %s to %s", old_filename, new_filename)
+        return old_filename.rename(new_filename)
+
+
+def first_retrieval_time_in_jsonl_file(live_file: Path) -> int:
+    with live_file.open(mode="r") as f:
+        first_line = f.readline()
+    first_line_dict: WrappedEnvoyData = json.loads(first_line)
+    return first_line_dict["retrieval_time"]
